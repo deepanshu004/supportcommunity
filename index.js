b6966c2fecae489ebbdbc70a1bfe5c3a314eacf4
localhost-cloud <deepanshuchahal358@gmail.com>
2026-05-08 00:26:54 +0530

    fix-bugs

--- index.js
+++ index.js
@@ -4,8 +4,13 @@ import simpleGit from "simple-git";
 import random from "random";
 
 const path = "./data.json";
+const TOTAL_COMMITS = 100;
 
-const markCommit = (x, y) => {
+const makeCommits = (n) => {
+  if (n === 0) return simpleGit().push();
+
+  const x = random.int(0, 51);
+  const y = random.int(0, 6);
   const date = moment()
     .subtract(1, "y")
     .add(1, "d")
@@ -13,28 +18,14 @@ const markCommit = (x, y) => {
     .add(y, "d")
     .format();
 
-  const data = {
-    date: date,
-  };
-
-  jsonfile.writeFile(path, data, () => {
-    simpleGit().add([path]).commit(date, { "--date": date }).push();
-  });
-};
-
-const makeCommits = (n) => {
-  if(n===0) return simpleGit().push();
-  const x = random.int(0, 54);
-  const y = random.int(0, 6);
-  const date = moment().subtract(1, "y").add(1, "d").add(x, "w").add(y, "d").format();
-
-  const data = {
-    date: date,
-  };
+  const data = { date };
   console.log(date);
+
   jsonfile.writeFile(path, data, () => {
-    simpleGit().add([path]).commit(date, { "--date": date },makeCommits.bind(this,--n));
+    simpleGit()
+      .add([path])
+      .commit(date, { "--date": date }, makeCommits.bind(this, n - 1));
   });
 };
 
-makeCommits(100);
+makeCommits(TOTAL_COMMITS);
